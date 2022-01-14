19328a851bc1b81265be547f9c03696045e8bdcf
Mathilde Renard <mathilderenard626@gmail.com>
2022-01-14 22:04:33 +0100

    mise à jour de la base de données

--- annuaire.sql
+++ annuaire.sql
@@ -1,36 +1,34 @@
 CREATE TABLE SITE(
-   id_site VARCHAR(50),
+   id_site INT NOT NULL AUTO_INCREMENT,
    name_site VARCHAR(50),
    PRIMARY KEY(id_site)
 );
 
 CREATE TABLE SERVICE(
-   id_service VARCHAR(50),
+   id_service INT NOT NULL AUTO_INCREMENT,
    name_service VARCHAR(50),
-   id_site VARCHAR(50) NOT NULL,
+   id_site INT,
    PRIMARY KEY(id_service),
    FOREIGN KEY(id_site) REFERENCES SITE(id_site)
 );
 
 CREATE TABLE ROLE(
-   id_role VARCHAR(50),
+   id_role INT NOT NULL AUTO_INCREMENT,
    role_name VARCHAR(50),
    PRIMARY KEY(id_role)
 );
 
 CREATE TABLE PERSON(
-   id_employee INT,
+   id_person INT NOT NULL AUTO_INCREMENT,
    firstname VARCHAR(50),
    lastname VARCHAR(50),
    phone INT,
    mobile_phone INT,
    mail VARCHAR(50),
    password VARCHAR(50),
-   id_role VARCHAR(50) NOT NULL,
-   id_service VARCHAR(50) NOT NULL,
-   id_site VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_employee),
+   id_role INT,
+   id_service INT,
+   PRIMARY KEY(id_person),
    FOREIGN KEY(id_role) REFERENCES ROLE(id_role),
-   FOREIGN KEY(id_service) REFERENCES SERVICE(id_service),
-   FOREIGN KEY(id_site) REFERENCES SITE(id_site)
+   FOREIGN KEY(id_service) REFERENCES SERVICE(id_service)
 );
