904aff146d326a3709288e299424903eb998dbea
Mathilde Renard <mathilderenard626@gmail.com>
2022-01-15 22:14:39 +0100

    CRUD service

--- annuaireEntreprise/annuaireEntreprise/Controllers/ServiceController.cs
+++ annuaireEntreprise/annuaireEntreprise/Controllers/ServiceController.cs
@@ -4,16 +4,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using annuaireEntreprise.Models;
 
 namespace annuaireEntreprise.Controllers
 {
     public class ServiceController : Controller
     {
         // GET: ServiceController1
-        public ActionResult Index()
+        public IActionResult Index()
         {
-            return View();
+
+            Service service = new Service();
+
+            return View(service.GetServices());
         }
+
 
         public ActionResult Result()
         {
@@ -23,6 +28,46 @@ namespace annuaireEntreprise.Controllers
         public ActionResult AdminService()
         {
             return View();
+        }
+        public ActionResult Add()
+
+        {
+            return View();
+
+        }
+        public ActionResult sendNew(string nomservice)
+
+        {
+            Service service = new Service();
+            service.Create(nomservice);
+            return RedirectToAction("Index", "Service");
+
+        }
+        //envoyer le nom du service, et l'id récupérée par le formulaire, à la vue de modification
+        public ActionResult Update(String nameservice, int id)
+
+        {
+            
+            ViewData["service"] = nameservice;
+            ViewData["id"] = id;
+            return View();
+
+        }
+        //envoyer les modifications à la méthode update
+        public ActionResult sendChanges(string nameservice, int id)
+
+        {
+            Service service = new Service();
+            service.Update(nameservice, id);
+            return RedirectToAction("Index", "Service");
+        }
+        public ActionResult Delete(int id)
+
+        {
+            Service service = new Service();
+            service.Delete(id);
+            return RedirectToAction("Index", "Service");
+
         }
     }
     }
--- annuaireEntreprise/annuaireEntreprise/Models/Service.cs
+++ annuaireEntreprise/annuaireEntreprise/Models/Service.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace annuaireEntreprise.Models
 {
@@ -11,19 +12,73 @@ namespace annuaireEntreprise.Models
         private string nameService;
         private List<Employee> employees;
         private Site site;
-        private Db db;
+        private string request;
+        private MySqlConnection connection = Db.Connection;
+        private MySqlCommand command;
+        private MySqlDataReader reader;
+        public int IdService { get => idService; set => idService = value; }
+        public string NameService { get => nameService; set => nameService = value; }
 
-        public void Create()
+        //Méthode qui permet la récupération de la liste des sites afin de les afficher
+        public List<Service> GetServices()
         {
+            List<Service> services = new List<Service>();
+            request = "SELECT id_service, name_service FROM service";
+
+            command = new MySqlCommand(request, connection);
+
+            connection.Open();
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Service service = new Service()
+                {
+                    IdService = reader.GetInt32(0),
+                    NameService = reader.GetString(1),
+
+                };
+                services.Add(service);
+            }
+
+            reader.Close();
+            command.Dispose();
+            connection.Close();
+            return services;
+        }
+        public void Create(string service)
+        {
+            request = "INSERT INTO service (name_service) VALUE(@nameService) ";
+            connection.Open();
+            command = new MySqlCommand(request, connection);
+            command.Parameters.Add(new MySqlParameter("@nameService", service));
+
+            command.ExecuteScalar();
+
+
+            command.Dispose();
+            connection.Close();
+        }
+        public void Update(string service, int id)
+        {
+            request = "UPDATE service SET name_service=@nameService WHERE id_service=@id ";
+            connection.Open();
+            command = new MySqlCommand(request, connection);
+            command.Parameters.Add(new MySqlParameter("@nameService", service));
+            command.Parameters.Add(new MySqlParameter("@id", id));
+            command.ExecuteScalar();
+            command.Dispose();
+            connection.Close();
 
         }
-        public void Update()
+        public void Delete(int id)
         {
-
-        }
-        public void Delete()
-        {
-
+            request = "DELETE FROM service WHERE id_service=@id";
+            connection.Open();
+            command = new MySqlCommand(request, connection);
+            command.Parameters.Add(new MySqlParameter("@id", id));
+            command.ExecuteScalar();
+            command.Dispose();
+            connection.Close();
         }
     }
 }
