220e86fc03fc3a4568ff8f32686f346bd0706450
Mathilde Renard <mathilderenard626@gmail.com>
2022-01-16 18:08:37 +0100

    barre de recherche

--- annuaireEntreprise/annuaireEntreprise/wwwroot/js/site.js
+++ annuaireEntreprise/annuaireEntreprise/wwwroot/js/site.js
@@ -166,7 +166,22 @@ function deroulant(id) {
     }
    
 }
-
+//Fonction pour la séléction dans la liste des services et sites sur la page de update et create pour les employés
 function ChangeValue(element, id) {
     document.getElementById('id' + element).value = id;
+}
+
+function search() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('site');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "block";
+        }
+    }
 }
