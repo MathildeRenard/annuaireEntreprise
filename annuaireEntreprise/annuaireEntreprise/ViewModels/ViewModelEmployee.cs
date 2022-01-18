ce1b47941b18113f14c7b6bc89b1ebdada6bc009
Mathilde Renard <mathilderenard626@gmail.com>
2022-01-18 11:18:14 +0100

    chiers manquants

---
+++ annuaireEntreprise/annuaireEntreprise/ViewModels/ViewModelEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using annuaireEntreprise.Models;
+
+namespace annuaireEntreprise.ViewModels
+{
+    //Cette classe permettra de passer deux paramètres à la vue update de employee
+    public class ViewModelEmployee
+    {
+        public List<Site> listeSites { get; set; }
+        public List<Service> listeServices { get; set; }
+       
+    }
+}
